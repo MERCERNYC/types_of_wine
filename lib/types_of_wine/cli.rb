ec5965485f3b439c5837ae4caffc48b3148dc371
Juliana Fogg <julianafogg@gmail.com>
2018-01-16 21:30:00 +0000

    add text to CLI

--- lib/types_of_wine/cli.rb
+++ lib/types_of_wine/cli.rb
@@ -1,5 +1,15 @@
 class TypesOfWine::CLI
   def call
-    puts "Welcome to Types of Wine is working people!"
+    puts "Welcome to Types of Wine!"
+    puts "Ever find yourself looking at a wine list, wanting a specific type of wine,
+    but don’t know how to ask for it? Learning how to order wine is as simple as having a good understanding of just 9 wine styles."
+    puts "_________________________"
+    puts "What types of wine would you more information on, please type number or name?"
+    puts "_________________________"
+    puts "Enter more to see the wine list again."
+    puts "Enter exit to end the program."
+    puts "_________________________"
   end
+
+
 end
