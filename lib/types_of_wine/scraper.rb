43e827f9402f4ecfab6a051b7947a01ffa3bde30
Juliana Fogg <julianafogg@gmail.com>
2018-01-19 05:31:04 +0000

    add scaper for wine name

--- lib/types_of_wine/scraper.rb
+++ lib/types_of_wine/scraper.rb
@@ -1,11 +1,12 @@
+require 'pry'
 class Scraper
 
   def self.wine_scraper
 
     doc = Nokogiri::HTML(open("http://winefolly.com/review/common-types-of-wine/"))
-
-    name = doc.search("div.row.span5 strong h3").text.strip
-    taste = doc.search(".span5 taste profile").text.strip
+    binding.pry
+    name = doc.search("div.span5 h3").text.strip
+    taste = doc.search("div.span5 icon-caret-right::before ").text.strip
     style = doc.search(".span5.icon-caret-right i strong").text.strip
     description = doc.search(".span5 strong").text.strip
     food_pairing = doc.search(".span5 strong").text.strip
