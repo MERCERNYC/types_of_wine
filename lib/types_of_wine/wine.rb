059b3460e95b765c4478ee61a2035aac86623c2b
github username <github email address>
2018-01-24 11:22:17 -0500

    created scraper file

--- lib/types_of_wine/wine.rb
+++ lib/types_of_wine/wine.rb
@@ -21,13 +21,15 @@ class TypesOfWine::Wine
     doc.search("div.span5 h3").each do |wines|
 
     wine = self.new
-    
+
     wine.spelling = doc.search("div.span5 p").children[0].text.strip #Select the first <1> element inside
     wine.taste = doc.search("div.span5 p").children[2].text.strip
     wine.style = doc.search("div.span5 p").children[6].text.strip
     wine.description = doc.search("div.span5 p").children[10].text.strip
     wine.food_pairing = doc.search("div.span5 p").children[16].text
     wine #return wine_scraper
+    end
   end
+
 
 end
